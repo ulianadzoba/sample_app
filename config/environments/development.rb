17bd94265099e2a9bffede64d9c8815e7d09eac6
Uliana Dzoba <ulianadzoba@gmail.com>
2020-03-03 13:08:28 +0200

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,11 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost', protocol: 'http', port: 3000 }
+ 
+  
 
   config.action_mailer.perform_caching = false
 
