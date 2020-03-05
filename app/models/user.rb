764057015990ef6cf78626486fd6be008ea4a66a
Uliana Dzoba <ulianadzoba@gmail.com>
2020-03-05 09:19:11 +0200

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save   :downcase_email
     before_create :create_activation_digest
@@ -52,8 +53,6 @@ class User < ApplicationRecord
     def create_reset_digest
         self.reset_token = User.new_token
         update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
-        # update_attribute(:reset_digest,  User.digest(reset_token))
-        # update_attribute(:reset_sent_at, Time.zone.now)
     end
 
     # Sends password reset email.
@@ -64,6 +63,10 @@ class User < ApplicationRecord
       # Returns true if a password reset has expired.
     def password_reset_expired?
         reset_sent_at < 2.hours.ago
+    end
+
+    def feed
+        Micropost.where("user_id = ?", id)
     end
 
     private 
